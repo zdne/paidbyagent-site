745f57ffd682bed71a0da72e1bd5e2583b5c3432
Zdenek Nemec <zdenek@me.com>
2026-09-22 21:29:59 +0200

    Add consulting visual assets and footer updates

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,8 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("site/favicon.svg");
   eleventyConfig.addPassthroughCopy("site/favicon.png");
   eleventyConfig.addPassthroughCopy("site/og-map.png");
+  eleventyConfig.addPassthroughCopy("site/agentic_payments_ecosystem_diagram@2x.png");
+  eleventyConfig.addPassthroughCopy("site/logos@2x.png");
 
   eleventyConfig.addFilter("readableDate", (value) => {
     const date = new Date(value);
